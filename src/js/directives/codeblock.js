c7f54e0f9c9dc6f0e2e852139c434c51b9f91960
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-10-28 04:30:29 +0100

    basic data-driven animation

--- src/js/directives/codeblock.js
+++ src/js/directives/codeblock.js
@@ -6,7 +6,7 @@ angular.module('ngBlog.directives')
             restrict: 'EA',
             scope: { src: "=src" },
             template: "<div class='codeblock'>"
-                + "<pre><code class='language-{{ mode }}'>{{ code }}</code></pre><div class='marker'></div></div>",
+                + "<pre><code class='language-{{ mode }}'>{{ code }}</code></pre><div style='top: {{ marker.top }}px; height: {{ marker.height }}px' class='marker'></div><div class='marker_description'>{{ marker.description }}</div></div>",
             link: function ($scope, elem, attrs) {
                 $http({method: 'GET', url: $scope.src, cache: false}).then(function (res) {
                     $scope.code = res.data
@@ -17,18 +17,29 @@ angular.module('ngBlog.directives')
                     var lineHeight = parseInt(elem.find('pre').css('line-height'));
                     var offset = 5;
 
-                    function getPos(line) { return (((line - 1) * lineHeight) + offset) + "px"; }
+                    $scope.marker = { top: 0, height: 0, description: "" };
 
-                    $timeout(function() { elem.find('.marker').css({top: getPos(2)}); }, 1000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(3)}); }, 2000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(4)}); }, 3000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(5)}); }, 4000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(6), height: "36px"}); }, 5000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(7)}); }, 6000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(8)}); }, 7000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(9), height: "18px"}); }, 8000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(12)}); }, 9000);
-                    $timeout(function() { elem.find('.marker').css({top: getPos(15), height: "72px"}); }, 10000);
+                    function getPos(line) { return ((line - 1) * lineHeight) + offset; }
+                    function getHeight(lines) {return lineHeight * lines + 1}
+
+                    var currentMarker = 0;
+                    var markers = [
+                        { line: 1, height: 3, duration: 2000, description: "Something is happening in line 4" },
+                        { line: 8, height: 7, duration: 4000, description: "line 8" },
+                        { line: 13, height: 2, duration: 2000, description: "wow, look at line 13" },
+                        { line: 6, height: 5, duration: 3000, description: "cool stuff in line 6" },
+                        { line: 9, height: 3, duration: 2000, description: "line 9 is interesting, too" },
+                    ];
+
+                    function curr() { return currentMarker % markers.length; }
+                    function next() {
+                        $scope.marker.top = getPos(markers[curr()].line);
+                        $scope.marker.height = getHeight(markers[curr()].height);
+                        $scope.marker.description = markers[curr()].description;
+                        currentMarker++;
+                        $timeout(next, markers[curr()].duration);
+                    }
+                    next();
                 });
             }
         }
