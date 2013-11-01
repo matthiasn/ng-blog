b01307bf48e23575ea287888dcb3982b6d4f506a
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-02 00:05:02 +0100

    basic updates on client working

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -3,6 +3,31 @@
 angular.module('ngBlog.controllers', [])
     .controller('blogCtrl', function ($scope, $http, $location, $timeout, $routeParams) {
         $scope.markdown = { src: "" };
+
+        $scope.load = function() {
+            if($scope.url) {
+                $http({method: 'GET', url: $scope.url + '.md', cache: false})
+                    .then(function (res) {
+                        $scope.markdown.src = res.data;
+                        $scope.markdown.url = $scope.url;
+                        $scope.markdown.lastEditFromAce = false;
+                    }, function (err) {
+                        $scope.markdown = { src: "#Sorry  \n There seems to be nothing at that address." };
+                        console.log(err)
+                        $timeout(function () {
+                             $location.url("/");
+                        }, 3000);
+                    }
+                );
+            }
+        };
+
+        var es = new EventSource("/sse");
+        es.onmessage = function (event) {
+            console.log(event.data);
+            $scope.load();
+        };
+
         var r = $routeParams;
         if (r.cat) {
             $scope.url = r.cat;
@@ -10,17 +35,6 @@ angular.module('ngBlog.controllers', [])
                 $scope.url = $scope.url + '/posts/' + r.year + '-' + r.month + '-' + r.day + '-' + r.title;
             }
             else { $scope.url = $scope.url + '/index'; }
-
-            $http({method: 'GET', url: $scope.url + '.md', cache: false})
-                .then(function (res) {
-                    $scope.markdown.src = res.data;
-                    $scope.markdown.url = $scope.url;
-                    $scope.markdown.lastEditFromAce = false;
-                }, function () {
-                    $scope.markdown = { src: "#Sorry  \n There seems to be nothing at that address." };
-                    $timeout(function () {
-                        $location.url("/");
-                    }, 3000);
-                });
+            $scope.load();
         }
     });
