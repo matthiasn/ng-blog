3dd7e219f20e7ad1b29bb84864f1c771fb22a027
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-03 13:27:39 +0100

    server cleanup, comments

--- scripts/server.js
+++ scripts/server.js
@@ -4,11 +4,12 @@
  *  Will serve files and additionally provide a server sent event (SSE) stream.
  *  In this stream: one event for each file change within the blog folder
  *
- *  My thanks go to:
+ *  Thanks go to:
  *  https://npmjs.org/package/sse
  *  https://github.com/paulmillr/chokidar
  *  https://gist.github.com/rpflorence/701407
  **/
+'use strict';
 
 var chok = require('chokidar');
 var SSE  = require('sse');
@@ -18,14 +19,13 @@ var path = require("path");
 var fs   = require("fs");
 var port = process.argv[2] || 8888;
 
-/** adapted from https://github.com/paulmillr/chokidar */
 var watcher = chok.watch('blog', { ignored: /^\./, persistent: true });
 
+/** basic web server, serving static files */
 var fileServer = http.createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname
-        , filename = path.join(process.cwd(), uri);
+    var filename = path.join(process.cwd(), url.parse(request.url).pathname);
 
-    path.exists(filename, function(exists) {
+    fs.exists(filename, function(exists) {
         if(!exists) {
             response.writeHead(404, {"Content-Type": "text/plain"});
             response.write("404 Not Found\n");
@@ -50,12 +50,10 @@ var fileServer = http.createServer(function(request, response) {
     });
 });
 
-fileServer.listen(8888, '127.0.0.1', function () {
+/** wraps basic web server in an SSE connection over which file system changes are broadcast */
+fileServer.listen(parseInt(port, 10), function () {
     var sse = new SSE(fileServer);
-    sse.on('connection', function (client) {
-        watcher.on('change', function (path) {
-            console.log(path);
-            client.send(path);
-        });
+    sse.on('connection', function (client) { // register watcher when connection starts
+        watcher.on('change', function (path) { client.send(path); }); // send path of changed file
     });
 });
