6ec0c8e1267264b2ad824d022c7a98de82683e7a
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-05 01:36:30 +0100

    image directive

--- src/js/directives/image.js
+++ src/js/directives/image.js
@@ -1,26 +1,34 @@
 'use strict';
 /** Image directive, loads appropriate image depending on size.
  *  Currently 3 sizes: normal, 600 pixels wide and 1600 pixels wide.
- *  Usage:  <div image name='blog/images/test.jpg'></div>
+ *  Usage: e.g. <span image="config.data.images.screenshot1" />
+ *  Element type not important, enclosing will be replaced
  */
 angular.module('ngBlog.directives')
-    .directive('image', function () {
+    .directive('image', function ($timeout) {
         return {
             restrict: 'EA',
-            scope: {},
-            template: "<img class='image' src='{{src}}' >",
+            scope: { image: "=image" },
+            templateUrl: "tpl/figure.tpl.html",
             replace: true,
-            link: function ($scope, elem, attrs) {
-                var file = attrs.name.split('.');
-                var suffix = "";
+            link: function ($scope, elem) {
+                // assign height style from cached configuration to avoid scroll issue in edit mode
+                if ($scope.hasOwnProperty("image.height") && $scope.image.height > 0) { $scope.style = { height: $scope.image.height + "px" }; }
 
+                // set element height in figure configuration
+                $timeout(function() {
+                    var height = elem.height();
+                    if (height > 0) { $scope.image.height = height; }
+                }, 3000);
+
+                var file = $scope.image.src.split('.');
+                var suffix = "";
                 if (window.devicePixelRatio) {
                     var width = window.devicePixelRatio * window.innerWidth;
                     if (width < 650) { suffix = "-600"; }
                     if (width > 1200) { suffix = "-1600"; }
                 }
-
-                $scope.src = "blog/images/" + file[0] + suffix + '.' + file[1];
+                $scope.src = file[0] + suffix + '.' + file[1];
             }
         }
     });
