29464e83cf7fb7e4ca219db50be4f9593e056b60
Matthias Nehlsen <mn@nehlsen-edv.de>
2013-11-08 02:39:10 +0100

    teasers directive; CSS

--- src/js/directives/teasers.js
+++ src/js/directives/teasers.js
@@ -2,23 +2,10 @@
 
 /** Teasers directive */
  angular.module('ngBlog.directives')
-    .directive('teasers', function ($compile, showdown) {
+    .directive('teasers', function () {
         return {
             restrict: 'A',
             scope: { config: "=config" },
-            templateUrl: "tpl/teasers.tpl.html",
-            link: function ($scope, elem) {
-
-//                var render = function () {
-//                    elem.empty().append($compile(showdown.makeHtml($scope.md.data))($scope));
-//                };
-//                $scope.$watch("md.data",  render);
-                var render = function() {
-
-                    console.log()
-                };
-
-                $scope.$watch("config.data", render);
-            }
+            templateUrl: "tpl/teasers.tpl.html"
         }
     });
